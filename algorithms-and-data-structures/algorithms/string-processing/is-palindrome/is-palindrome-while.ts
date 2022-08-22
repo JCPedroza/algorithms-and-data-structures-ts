8e4435a1d43cdd0689c66e50dbba30685d5acd7d
JCPedroza <nemesis4go10@hotmail.com>
2022-08-22 10:57:37 -0500

    refa: add comments

--- algorithms-and-data-structures/algorithms/string-processing/is-palindrome/is-palindrome-while.ts
+++ algorithms-and-data-structures/algorithms/string-processing/is-palindrome/is-palindrome-while.ts
@@ -1,21 +1,23 @@
 /**
- * Check if string is a palindrome using a while loop.
+ * Check if a given string is a palindrome.
+ * Uses a symmetric while loop.
  * @param str String to be checked.
  * @returns True if the string is a palindrome, false otherwise.
  */
 const isPalindrome = (str: string): boolean => {
   let leftIndex = 0
   let rightIndex = str.length - 1
-  const notWord = /[\W]/
+  const notWord = /[\W]/ // Character class \W represents non-alphanumeric characters
 
   while (leftIndex < rightIndex) {
     if (notWord.test(str[leftIndex])) {
-      leftIndex++
+      leftIndex++ // Left char is not alphanumeric
     } else if (notWord.test(str[rightIndex])) {
-      rightIndex--
+      rightIndex-- // Right char is not alphanumeric
     } else if (str[leftIndex].toLowerCase() !== str[rightIndex].toLowerCase()) {
-      return false
+      return false // Both characters are different
     } else {
+      // Both characters are equal
       leftIndex++
       rightIndex--
     }
@@ -26,7 +28,7 @@ const isPalindrome = (str: string): boolean => {
 
 const algorithm = {
   fun: isPalindrome,
-  id: 'while loop'
+  id: 'symmetric while loop'
 }
 
 export default algorithm
