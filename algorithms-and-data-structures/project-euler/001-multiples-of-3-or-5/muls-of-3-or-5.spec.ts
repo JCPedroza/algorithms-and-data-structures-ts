41bb9705b5d00dc52125a39f28e6024727298c86
JCPedroza <nemesis4go10@hotmail.com>
2022-08-22 05:32:07 -0500

    test: use more concise 'it' string

--- algorithms-and-data-structures/project-euler/001-multiples-of-3-or-5/muls-of-3-or-5.spec.ts
+++ algorithms-and-data-structures/project-euler/001-multiples-of-3-or-5/muls-of-3-or-5.spec.ts
@@ -7,8 +7,8 @@ const testSubjects = [
 ]
 
 for (const { fun, id } of testSubjects) {
-  describe(`Project Euler 001 "multiples of 3 or 5" solution "${id}"`, () => {
-    it('computes the sum below a given input', () => {
+  describe(`Project Euler 001 solution "${id}"`, () => {
+    it('computes the sum of the natural multiples of 3 or 5 < limit', () => {
       expect(fun(-1)).toBe(0)
       expect(fun(0)).toBe(0)
       expect(fun(3)).toBe(0)
