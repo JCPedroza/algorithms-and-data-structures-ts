a2332244fefed50ea2acb5e83368276df8486488
JCPedroza <nemesis4go10@hotmail.com>
2022-08-22 02:49:17 -0500

    test: don't generalize expects

--- algorithms-and-data-structures/data-structures/queue/queue.spec.ts
+++ algorithms-and-data-structures/data-structures/queue/queue.spec.ts
@@ -5,40 +5,43 @@ const testSubjects = [
   ArrayQueue
 ]
 
-const checkState = <T>(
-  queue: QueueADT<T>,
-  peek: T | undefined,
-  length: number,
-  isEmpty: boolean
-): void => {
-  expect(queue.peek()).toEqual(peek)
-  expect(queue.length()).toBe(length)
-  expect(queue.isEmpty()).toBe(isEmpty)
-}
-
 for (const Queue of testSubjects) {
   describe(`Queue structure "${Queue.id}"`, () => {
     it('performs basic queue operations', () => {
       const queue = new Queue<number>()
-      checkState(queue, undefined, 0, true)
+      expect(queue.peek()).toEqual(undefined)
+      expect(queue.length()).toBe(0)
+      expect(queue.isEmpty()).toBe(true)
 
       expect(queue.dequeue()).toBe(undefined)
-      checkState(queue, undefined, 0, true)
+      expect(queue.peek()).toEqual(undefined)
+      expect(queue.length()).toBe(0)
+      expect(queue.isEmpty()).toBe(true)
 
       expect(queue.enqueue(2)).toBe(1)
-      checkState(queue, 2, 1, false)
+      expect(queue.peek()).toEqual(2)
+      expect(queue.length()).toBe(1)
+      expect(queue.isEmpty()).toBe(false)
 
       expect(queue.enqueue(4)).toBe(2)
-      checkState(queue, 2, 2, false)
+      expect(queue.peek()).toEqual(2)
+      expect(queue.length()).toBe(2)
+      expect(queue.isEmpty()).toBe(false)
 
       expect(queue.dequeue()).toBe(2)
-      checkState(queue, 4, 1, false)
+      expect(queue.peek()).toEqual(4)
+      expect(queue.length()).toBe(1)
+      expect(queue.isEmpty()).toBe(false)
 
       expect(queue.dequeue()).toBe(4)
-      checkState(queue, undefined, 0, true)
+      expect(queue.peek()).toEqual(undefined)
+      expect(queue.length()).toBe(0)
+      expect(queue.isEmpty()).toBe(true)
 
       expect(queue.dequeue()).toBe(undefined)
-      checkState(queue, undefined, 0, true)
+      expect(queue.peek()).toEqual(undefined)
+      expect(queue.length()).toBe(0)
+      expect(queue.isEmpty()).toBe(true)
     })
   })
 }
