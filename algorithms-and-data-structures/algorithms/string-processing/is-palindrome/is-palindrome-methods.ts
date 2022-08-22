4ab6b634417377ca266fc23f6355c16c1ac498e2
JCPedroza <nemesis4go10@hotmail.com>
2022-08-22 11:00:06 -0500

    refa: add comments

--- algorithms-and-data-structures/algorithms/string-processing/is-palindrome/is-palindrome-methods.ts
+++ algorithms-and-data-structures/algorithms/string-processing/is-palindrome/is-palindrome-methods.ts
@@ -1,9 +1,11 @@
 /**
- * Check if string is a palindrome using string and array methods.
+ * Check if a given string is a palindrome
+ * Uses string and array methods.
  * @param str String to be checked.
  * @returns True if the string is a palindrome, false otherwise.
  */
 const isPalindrome = (str: string): boolean => {
+  // Character class \W represents non-alphanumeric characters
   const normalStr = str.replace(/\W/g, '').toLowerCase()
   const reverseStr = [...normalStr].reverse().join('')
   return normalStr === reverseStr
