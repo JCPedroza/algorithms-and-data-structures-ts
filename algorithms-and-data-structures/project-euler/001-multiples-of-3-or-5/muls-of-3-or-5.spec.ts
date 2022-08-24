762feabcaa205c6826eebcceb65daf9eb7c32f08
JCPedroza <nemesis4go10@hotmail.com>
2022-08-24 08:39:13 -0500

    refa: use file name for binding

--- algorithms-and-data-structures/project-euler/001-multiples-of-3-or-5/muls-of-3-or-5.spec.ts
+++ algorithms-and-data-structures/project-euler/001-multiples-of-3-or-5/muls-of-3-or-5.spec.ts
@@ -1,10 +1,10 @@
-import mulsOfTrialFor from './muls-of-3-or-5-brute-for'
+import mulsOfBruteFor from './muls-of-3-or-5-brute-for'
 import mulsOfSkipWhile from './muls-of-3-or-5-skip-while'
 import mulsOfAnalytic from './muls-of-3-or-5-analytic'
 
 const testSubjects = [
   mulsOfSkipWhile,
-  mulsOfTrialFor,
+  mulsOfBruteFor,
   mulsOfAnalytic
 ]
 
