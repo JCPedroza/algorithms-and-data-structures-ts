32f761cb8de1605284c3bd99cc2fe0ddba7279b2
JCPedroza <nemesis4go10@hotmail.com>
2022-08-24 10:25:35 -0500

    opti: use two sequential while loops

--- algorithms-and-data-structures/project-euler/001-multiples-of-3-or-5/muls-of-3-or-5-skip-while.ts
+++ algorithms-and-data-structures/project-euler/001-multiples-of-3-or-5/muls-of-3-or-5-skip-while.ts
@@ -1,6 +1,6 @@
 /**
  * Compute the sum of the natural multiples of 3 or 5 that are smaller than a limit.
- * Uses: Skip trial division with a while loop.
+ * Uses skip trial division with while loops.
  * Complexity: time O(n), space O(1).
  * @param limit Exclusive limit of the series.
  * @return Sum of the natural multiples of 3 or 5 that are smaller than the limit.
@@ -10,16 +10,17 @@ const mulsOf3Or5 = (limit: number): number => {
   let mul5 = 5
   let sum = 0
 
-  // Only check limit for multiples of 3, but multiples of 5 are set to 0 once they
-  // reach their limit.
-  while (mul3 < limit) {
-    if (mul5 >= limit) mul5 = 0 // Don't count multiples of 5 above the limit
-
+  while (mul5 < limit) {
     sum += mul3
     if (mul5 % 3 !== 0) sum += mul5 // Don't count multiples of both 3 and 5 twice
 
     mul3 += 3
-    if (mul5 > 0) mul5 += 5 // Increase if multiples of 5 haven't reached their limit
+    mul5 += 5
+  }
+
+  while (mul3 < limit) {
+    sum += mul3
+    mul3 += 3
   }
 
   return sum
@@ -27,7 +28,7 @@ const mulsOf3Or5 = (limit: number): number => {
 
 const solution = {
   fun: mulsOf3Or5,
-  id: 'skip trial division with while loop'
+  id: 'skip trial division with while loops'
 }
 
 export default solution
