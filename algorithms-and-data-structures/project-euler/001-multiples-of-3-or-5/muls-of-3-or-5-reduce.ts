a7ae79d6f2fc29f7cfcd595c228d3b53572a4f38
JCPedroza <nemesis4go10@hotmail.com>
2022-08-24 10:09:58 -0500

    fix: use || for filter

--- algorithms-and-data-structures/project-euler/001-multiples-of-3-or-5/muls-of-3-or-5-reduce.ts
+++ algorithms-and-data-structures/project-euler/001-multiples-of-3-or-5/muls-of-3-or-5-reduce.ts
@@ -1,14 +1,14 @@
 /**
  * Compute the sum of the natural multiples of 3 or 5 that are smaller than a limit.
  * Uses: Filter reduce..
- * Complexity: time O(n)? exponentiation?, space O(1).
+ * Complexity: time O(n) space O(n).
  * @param limit Exclusive limit of the series.
  * @return Sum of the natural multiples of 3 or 5 that are smaller than the limit.
  */
 const mulsOf3Or5 = (limit: number): number =>
-  [...Array(limit + 1).keys()]
-    .filter(num => num % 3 === 0 && num % 5 === 0)
-    .reduce((sum, num) => sum + num)
+  [...Array(limit < 0 ? 0 : limit).keys()]
+    .filter(num => num % 3 === 0 || num % 5 === 0)
+    .reduce((sum, num) => sum + num, 0)
 
 const solution = {
   fun: mulsOf3Or5,
