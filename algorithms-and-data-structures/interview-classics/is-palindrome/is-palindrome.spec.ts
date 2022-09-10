222af299ebedf2298b863a63e6351f31a1a2a2c6
JCPedroza <nemesis4go10@hotmail.com>
2022-09-10 01:47:39 -0500

    test: add missing test case scenarios

--- algorithms-and-data-structures/interview-classics/is-palindrome/is-palindrome.spec.ts
+++ algorithms-and-data-structures/interview-classics/is-palindrome/is-palindrome.spec.ts
@@ -4,17 +4,21 @@ for (const { fun, id } of solutions) {
   describe(`Is-palindrome algorithm "${id}"`, () => {
     it('returns true if the input string is a palindrome', () => {
       expect(fun('')).toBe(true)
+      expect(fun(' ')).toBe(true)
       expect(fun('1')).toBe(true)
       expect(fun('22')).toBe(true)
       expect(fun('121')).toBe(true)
       expect(fun('TeNEt')).toBe(true)
       expect(fun('!? R . a : ceca;r')).toBe(true)
       expect(fun('¿Ani. ta?  ..  LAva :l;a tina!')).toBe(true)
+      expect(`mUltiline!Example
+              elpmaXe=enilitluM`)
     })
 
     it('returns false if the input string is not a palindrome', () => {
       expect(fun('01')).toBe(false)
       expect(fun('Not a palindrome')).toBe(false)
+      expect(fun('.?a; b')).toBe(false)
     })
   })
 }
