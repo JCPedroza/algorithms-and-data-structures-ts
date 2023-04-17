37b27dcd2c9af6bdbfeb8c8a9a1b441bbc2ab0c1
JCPedroza <nemesis4go10@hotmail.com>
2023-04-16 22:51:49 -0600

    docs: add function docstring

--- algorithms-and-data-structures/algorithms/math/integer-exponentiation/int-exp-native.ts
+++ algorithms-and-data-structures/algorithms/math/integer-exponentiation/int-exp-native.ts
@@ -2,7 +2,7 @@
  * Integer exponentiation.
  * Uses the native power function.
  * Complexity: depends on implementation.
- * 
+ *
  * @param base Base that will be taken to the given power (must be integer).
  * @param pow The base will be taken to this power (must be integer).
  * @return The base taken to the power.
--- algorithms-and-data-structures/algorithms/math/integer-exponentiation/int-exp-square.ts
+++ algorithms-and-data-structures/algorithms/math/integer-exponentiation/int-exp-square.ts
@@ -4,7 +4,7 @@
  * Integer exponentiation.
  * Uses exponentiation by squaring.
  * Complexity: time O(log n), space O(log n).
- * 
+ *
  * @param base Base that will be taken to the given power (must be integer).
  * @param pow The base will be taken to this power (must be integer).
  * @return The base taken to the power.
