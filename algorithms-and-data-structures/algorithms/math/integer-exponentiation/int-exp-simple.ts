6d926e0eb63617de968ee8f28ee75a3ce54b6838
JCPedroza <nemesis4go10@hotmail.com>
2023-04-16 22:41:58 -0600

    docs: add function docstring

--- algorithms-and-data-structures/algorithms/math/integer-exponentiation/int-exp-simple.ts
+++ algorithms-and-data-structures/algorithms/math/integer-exponentiation/int-exp-simple.ts
@@ -1,3 +1,12 @@
+/**
+ * Integer exponentiation.
+ * Uses a brute-force approach.
+ * Complexity: time O(n), space O(1).
+ * 
+ * @param base Base that will be taken to the given power (must be integer).
+ * @param pow The base will be taken to this power (must be integer).
+ * @return The base taken to the power.
+ */
 const intExp = (base: number, pow: number): number => {
   if (!Number.isInteger(base) || !Number.isInteger(pow)) {
     throw new Error(`Non-integer argument (base: ${base} pow: ${pow})`)
--- algorithms-and-data-structures/algorithms/math/integer-exponentiation/int-exp-square.ts
+++ algorithms-and-data-structures/algorithms/math/integer-exponentiation/int-exp-square.ts
@@ -1,15 +1,24 @@
 // https://en.wikipedia.org/wiki/Exponentiation_by_squaring
 
+/**
+ * Integer exponentiation.
+ * Uses exponentiation by squaring.
+ * Complexity: time O(log n), space O(log n).
+ * 
+ * @param base Base that will be taken to the given power (must be integer).
+ * @param pow The base will be taken to this power (must be integer).
+ * @return The base taken to the power.
+ */
 const intExp = (base: number, pow: number): number => {
   if (!Number.isInteger(base) || !Number.isInteger(pow)) {
     throw new Error(`Non-integer argument (base: ${base} pow: ${pow})`)
   }
 
   const loop = (lbase: number, lpow: number): number => {
-    if (lpow < 0) {
-      return loop(1 / lbase, -lpow)
-    } else if (lpow === 0) {
+    if (lpow === 0) {
       return 1
+    } else if (lpow < 0) {
+      return loop(1 / lbase, -lpow)
     } else if (lpow % 2 === 0) {
       return loop(lbase * lbase, lpow / 2)
     } else {
